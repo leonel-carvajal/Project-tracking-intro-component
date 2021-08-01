47fc43a65d7ecb69101826764e9cd4567462fd19
Leonel Carvajal <liosoft777@gmail.com>
2021-07-31 19:28:30 -0600

    Menu hamburguer

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const Menu = document.querySelector('.header__menu')
+const IconMenu = document.querySelector('.header__IconMenu')
+
+const MenuChange = (e)=>{
+  if(!Menu.classList.contains('active')){
+    Menu.classList.add('active')
+    IconMenu.setAttribute('src','images/icon-close.svg')
+  }else{
+    Menu.classList.remove('active')
+    IconMenu.setAttribute('src','images/icon-hamburger.svg')
+  }
+}
+
+IconMenu.addEventListener('click',MenuChange)
